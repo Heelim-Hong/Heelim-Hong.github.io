51c51cfa7dd2186ebc1f0cfd947b84f3fdc1f18b
heelim-hong <heelim.korea@gmail.com>
2022-01-26 17:28:46 +0900

    update bundle install

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "[Jekyll] 블로그 포스팅하는 방법",
+        "excerpt":"1. Github 에서 블로그 용으로 쓸 새로운 Repository 를 생성한다. 레포지토리의 이름을 자신의 깃허브 계정 이름.github.io 로 하여 생성해준다. ex) ansohxxn.github.io 2. 생성한 Repository를 Local 환경으로 Clone 해 온다. 명령 프롬프트 cmd 를 실행하여 원하는 위치로 이동한다. 나의 경우 D드라이브에 설치하기 위해 cmd에 D: 를 입력 후 엔터 쳐 D드라이브로...","categories": ["Blog"],
+        "tags": ["Blog","jekyll","Github","Git","minimal-mistake"],
+        "url": "/blog/my-first-post/",
+        "teaser": null
+      }]
